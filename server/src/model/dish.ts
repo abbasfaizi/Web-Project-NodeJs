048197f79981e23f7429df115925f8fd1b12cb6c
Mustafa <bawim@student.chalmers.se>
2023-01-28 15:39:10 +0100

    Added a dish entity in model layer. Read comments!
    Co-authored-by: abbas <faizi@student.chalmers.se>

---
+++ server/src/model/dish.ts
@@ -0,0 +1,36 @@
+import { User } from "./user";
+
+export class Dish {
+
+    id : number;
+    name : string;
+    imageUrl: string; //Url to the image of the food.
+    likedBy: User[];
+    dislikedBy: User[];  //So the dislike and like should obviously be referenced to the user entity, i think.
+
+    constructor( id: number, name: string, imageUrl: string) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.likedBy = [];
+        this.dislikedBy = [];
+    }
+
+    like(user:User) {
+        this.likedBy.push(user); //push a user to the "likedBy" array
+    }
+
+    dislike(user:User) {
+        this.dislikedBy.push(user); //push a user to the "dislikedBy" array
+    }
+}
+
+
+
+
+
+
+
+
+
+
