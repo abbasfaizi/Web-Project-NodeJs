8f0e0fd221eb3d3c1f8b5bddfadbe0f07936862b
abbasfaizi <abbasfaizi.af@gmail.com>
2023-01-29 13:24:19 +0100

    did a test for like and dislike the dishes

--- server/src/service/test.ts
+++ server/src/service/test.ts
@@ -1,1 +1,29 @@
 //unit test folder, at least one test is required for the lab
+
+import { Dish } from '../model/dish';
+import { User } from '../model/user';
+
+describe('Dish', () => {
+  let dish: Dish;
+  let user: User;
+
+  beforeEach(() => {
+    dish = new Dish(1, 'Pizza', 'https://example.com/pizza.jpg');
+    user = new User(1, 'Abbas Faizi','abbas@gmail.com');
+  });
+
+  it('should have empty arrays for likedBy and dislikedBy', () => {
+    expect(dish.likedBy).toEqual([]);
+    expect(dish.dislikedBy).toEqual([]);
+  });
+
+  it('should be able to like a dish', () => {
+    dish.like(user);
+    expect(dish.likedBy).toEqual([user]);
+  });
+
+  it('should be able to dislike a dish', () => {
+    dish.dislike(user);
+    expect(dish.dislikedBy).toEqual([user]);
+  });
+});
