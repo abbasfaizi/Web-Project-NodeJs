6ff0867c6457f149e95590ff3bc9429b46a11abb
abbasfaizi <abbasfaizi.af@gmail.com>
2023-01-29 12:48:35 +0100

    made some changes in user.ts

--- server/src/model/user.ts
+++ server/src/model/user.ts
@@ -6,12 +6,18 @@ export class User {
     name: string;
     //Maybe some extra identifiers, i don't know?
 
+    email: string;
+    password: string;
 
-constructor(id: number, name: string) {
+constructor(id: number, name: string, email: string, password:string) {
     this.id = id;       //This code was taken from the intial example in the assigments page.
     this.name = name;    
+    this.email = email;
+    this.password = password;
     
 }
+
+
 }
 
 //We will probably have more entities like "Dish" and "Match" to represent the food and the matches between users.
