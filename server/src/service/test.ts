d65940f3464449318cae728f7cd31a3b7e75fcbd
Love-Ry1 <83970045+Love-Ry1@users.noreply.github.com>
2023-02-05 13:51:16 +0100

    Update test.ts
    updated tests
    
    Co-Authored-By: Civiliangem0 <18362385+Civiliangem0@users.noreply.github.com>
    Co-Authored-By: abbasfaizi <52606473+abbasfaizi@users.noreply.github.com>
    Co-Authored-By: Zakariya Omar <100076090+Zakariya00@users.noreply.github.com>
    Co-Authored-By: Mustafa0134 <89937556+Mustafa0134@users.noreply.github.com>

--- server/src/service/test.ts
+++ server/src/service/test.ts
@@ -4,26 +4,26 @@ import { Restaurants } from '../model/restaurants';
 import { User } from '../model/user';
 
 describe('Dish', () => {
-  let dish: Restaurants;
+  let restaurant: Restaurants;
   let user: User;
 
   beforeEach(() => {
-    dish = new Restaurants(1, 'Pizza', 'https://example.com/pizza.jpg');
+    restaurant = new Restaurants(1, 'Pizza', 'https://example.com/pizza.jpg');
     user = new User(1, 'Abbas Faizi','abbas@gmail.com', "mypassword");
   });
 
   it('should have empty arrays for likedBy and dislikedBy', () => {
-    expect(dish.likedBy).toEqual([]);
-    expect(dish.dislikedBy).toEqual([]);
+    expect(user.liked).toEqual([]);
+    expect(user.disliked).toEqual([]);
   });
 
   it('should be able to like a dish', () => {
-    dish.like(user);
-    expect(dish.likedBy).toEqual([user]);
+    user.like(restaurant);
+    expect(user.liked).toEqual([user]);
   });
 
   it('should be able to dislike a dish', () => {
-    dish.dislike(user);
-    expect(dish.dislikedBy).toEqual([user]);
+    user.dislike(restaurant);
+    expect(user.disliked).toEqual([user]);
   });
 });
