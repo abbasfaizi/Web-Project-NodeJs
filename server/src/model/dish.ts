e3b3bc357aacc550691da49b635d2a7aff97a87b
Love-Ry1 <83970045+Love-Ry1@users.noreply.github.com>
2023-02-05 13:29:17 +0100

    Changed properties
    Liked/disklike restaurants is stores in user.
    
    Co-Authored-By: Civiliangem0 <18362385+Civiliangem0@users.noreply.github.com>
    Co-Authored-By: abbasfaizi <52606473+abbasfaizi@users.noreply.github.com>

--- server/src/model/dish.ts
+++ server/src/model/dish.ts
@@ -5,22 +5,11 @@ export class Dish {
     id : number;
     name : string;
     imageUrl: string; //Url to the image of the food.
-    likedBy: User[];
-    dislikedBy: User[];  //So the dislike and like should obviously be referenced to the user entity, i think.
 
     constructor( id: number, name: string, imageUrl: string) {
         this.id = id;
         this.name = name;
         this.imageUrl = imageUrl;
-        this.likedBy = [];
-        this.dislikedBy = [];
     }
 
-    like(user:User) {
-        this.likedBy.push(user); //push a user to the "likedBy" array
-    }
-
-    dislike(user:User) {
-        this.dislikedBy.push(user); //push a user to the "dislikedBy" array
-    }
 }
--- server/src/model/user.ts
+++ server/src/model/user.ts
@@ -1,22 +1,36 @@
-//So in the model layer one of our entities will definitely be the user so i'm just writing a skeleton for it and trying it out. -- Mustafa
+// So in the model layer one of our entities will definitely be the user, so I am just writing a skeleton for it and trying it out. -- Mustafa
+import {Dish} from "./dish";
 
 export class User {
 
     id: number;
     name: string;
-    //Maybe some extra identifiers, i don't know?
+    // Maybe some extra identifiers, I don't know?
     email: string;
     password: string;
+    // Stores what restaurants a user likes/dislikes
+    liked: Dish[];
+    dislike: Dish[];
 
-constructor(id: number, name: string, email: string, password:string) {
-    this.id = id;       //This code was taken from the intial example in the assigments page.
-    this.name = name;    
-    this.email = email;
-    this.password = password;
-    
+    constructor(id: number, name: string, email: string, password:string) {
+        this.id = id;       //This code was taken from the initial example in the assignments page.
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.liked = [];
+        this.dislike = [];
+    }
+
+    // push a dish (restaurant) to the dislike-array
+    like(dish:Dish) {
+        this.liked.push(dish);
+    }
+
+    // push a dish (restaurant) to the liked-array
+   dislike(dish:Dish) {
+        this.dislike.push(dish);
+    }
+
 }
 
-
-}
-
-//We will probably have more entities like "Dish" and "Match" to represent the food and the matches between users.
+// We will probably have more entities like "Dish" and "Match" to represent the food and the matches between users.
