e8bde57df6619cbee623dc7244d3173709ff166a
Abbas Faizi <abbasfaizi.af@gmail.com>
2023-02-11 23:10:38 +0100

    added cors to start.ts
    Co-authored-by: Zakariya00 <100076090+Zakariya00@users.noreply.github.com>
    Co-authored-by: Mustafa <bawim@student.chalmers.se>
    Co-authored-by: Love-Ry1 <83970045+Love-Ry1@users.noreply.github.com>
    Co-authored-by: Civiliangem0 <fadd@live.se>

--- server/src/start.ts
+++ server/src/start.ts
@@ -7,6 +7,7 @@ import express from "express";
 import { restaurantRouter } from "./router/restaurant.router";
 import { userRouter } from "./router/user.router";
 import {defaultRouter} from "./router/default.router";
+import cors from "cors";
 
 
 /**
@@ -24,6 +25,7 @@ export const app = express();
 
 
 app.use(express.json());
+app.use(cors());
 app.use("/restaurant", restaurantRouter);  // Example: http://localhost:8080/dish
 app.use("/user", userRouter); //  Example: http://localhost:8080/user
 app.use("*", defaultRouter); //   Example: http://localhost:8080
