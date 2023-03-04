586b359b4c43ff273b9e9e676648fc0e14428b63
AbbasFaizi <abbasfaizi.af@gmail.com>
2023-03-04 11:32:16 +0100

    test for add, get and check restaurants
    Co-authored-by: Mustafa0134 <89937556+Mustafa0134@users.noreply.github.com>
    Co-authored-by: Civiliangem0 <fadd@live.se>

--- server/src/service/test.ts
+++ server/src/service/test.ts
@@ -1,29 +1,67 @@
-//unit test folder, at least one test is required for the lab
 
-import { Restaurants } from '../model/restaurants';
-import { User } from '../model/user';
 
-describe('Restaurant', () => {
-  let restaurant: Restaurants;
-  let user: User;
+
+//This test suite covers the getRestaurants, getRestaurant, addRestaurant, and checkRestaurant methods of the RestaurantService class, and ensures that they behave as expected.
+import { Restaurants } from "../model/restaurants";
+import { IRestaurantService, makeRestaurantService } from "./restaurant.service";
+
+describe("RestaurantService", () => {
+  let restaurantService: IRestaurantService;
 
   beforeEach(() => {
-    restaurant = new Restaurants(1, 'Pizza', 'https://example.com/pizza.jpg');
-    user = new User(1, 'Abbas Faizi','abbas@gmail.com', "mypassword");
+    restaurantService = makeRestaurantService();
   });
 
-  it('should have empty arrays for likedBy and dislikedBy', () => {
-    expect(user.liked).toEqual([]);
-    expect(user.disliked).toEqual([]);
+  describe("getRestaurants", () => {
+    it("returns an empty Map when no restaurants have been added", async () => {
+      const restaurants = await restaurantService.getRestaurants();
+      expect(restaurants.size).toBe(0);
+    });
+
+    it("returns a Map with one restaurant when one restaurant has been added", async () => {
+      const restaurant: Restaurants = new Restaurants(1, "Test Restaurant", "http://test.com/image.jpg");
+      await restaurantService.addRestaurant(restaurant);
+      const restaurants = await restaurantService.getRestaurants();
+      expect(restaurants.size).toBe(1);
+      expect(restaurants.get(1)).toEqual(restaurant);
+    });
   });
 
-  it('should be able to like a dish', () => {
-    user.like(restaurant);
-    expect(user.liked).toEqual([user]);
+  describe("getRestaurant", () => {
+    it("throws an error when the restaurant is not found", async () => {
+      await expect(restaurantService.getRestaurant(1)).rejects.toThrow();
+    });
+
+    it("returns the correct restaurant when the restaurant is found", async () => {
+      const restaurant: Restaurants = new Restaurants(1, "Test Restaurant", "http://test.com/image.jpg");
+      await restaurantService.addRestaurant(restaurant);
+      const result = await restaurantService.getRestaurant(1);
+      expect(result).toEqual(restaurant);
+    });
   });
 
-  it('should be able to dislike a dish', () => {
-    user.dislike(restaurant);
-    expect(user.disliked).toEqual([user]);
+  describe("addRestaurant", () => {
+    it("adds a restaurant to the Map and returns the same restaurant", async () => {
+      const restaurant: Restaurants = new Restaurants(1, "Test Restaurant", "http://test.com/image.jpg");
+      const result = await restaurantService.addRestaurant(restaurant);
+      const restaurants = await restaurantService.getRestaurants();
+      expect(restaurants.size).toBe(1);
+      expect(restaurants.get(1)).toEqual(restaurant);
+      expect(result).toEqual(restaurant);
+    });
+  });
+
+  describe("checkRestaurant", () => {
+    it("returns false when the restaurant is not found", async () => {
+      const result = await restaurantService.checkRestaurant(1);
+      expect(result).toBe(false);
+    });
+
+    it("returns true when the restaurant is found", async () => {
+      const restaurant: Restaurants = new Restaurants(1, "Test Restaurant", "http://test.com/image.jpg");
+      await restaurantService.addRestaurant(restaurant);
+      const result = await restaurantService.checkRestaurant(1);
+      expect(result).toBe(true);
+    });
   });
 });
