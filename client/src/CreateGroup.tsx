93e6315e767f0c94578df5dc1e4080c49c52615e
Love-Ry1 <83970045+Love-Ry1@users.noreply.github.com>
2023-03-10 13:29:16 +0100

    Added alert when sucesfully joining/creating a group

--- client/src/CreateGroup.tsx
+++ client/src/CreateGroup.tsx
@@ -5,23 +5,29 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './CreateGroup.css'
 import axios from "axios";
 
-async function onClickedCreate(id : string, password : string, location : string){
-    const response = await axios.post("http://localhost:8080/group/create", {id : id, password : password, location : location});
-    console.log(response.data);
-}
-
-
 function CreateGroup(props : {
     goToMainPage : () => void;
 }) {
     const [id, setId] = useState('');
     const [password, setPassword] = useState('');
     const [location, setLocation] = useState('');
+    const [showAlert, setShowAlert] = useState(false);
+
+    async function onClickedCreate(id : string, password : string, location : string){
+        const response = await axios.post("http://localhost:8080/group/create", {id : id, password : password, location : location});
+        console.log(response.data);
+        if (response.status === 201) {
+            setShowAlert(true);
+        }
+    }
 
     return (
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-md-6">
+                    <div className={`alert alert-success ${showAlert ? "d-block" : "d-none"}`} role="alert">
+                        Group created successfully!
+                    </div>
                     <div className="card mt-5">
                         <div className="card-header">
                             <img src={logo} className="Web-Logo" alt="Logo" />
--- client/src/JoinGroupPage.tsx
+++ client/src/JoinGroupPage.tsx
@@ -1,20 +1,28 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import logo from './images/logoImage.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './JoinGroupPage.css'
 import axios from "axios";
 
-async function onClickedJoin(id : string, password : string){
-    const response = await axios.post("http://localhost:8080/group/join", {id : id, password : password});
-    console.log(response.data);
-}
-
-function JoinGroupPage(props : {
-    goToMainPage : () => void;
-}){
+function JoinGroupPage(props: {
+    goToMainPage: () => void;
+}) {
     const [id, setId] = useState('');
     const [password, setPassword] = useState('');
+    const [showAlert, setShowAlert] = useState(false);
+
+    async function onClickedJoin(id: string, password: string) {
+        try {
+            const response = await axios.post("http://localhost:8080/group/join", { id: id, password: password });
+            console.log(response.data);
+            if (response.status === 201) {
+                setShowAlert(true);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
         <div className="container">
@@ -25,6 +33,11 @@ function JoinGroupPage(props : {
                             <img src={logo} className="Web-Logo" alt="Logo" />
                         </div>
                         <div className="card-body">
+                            {showAlert && (
+                                <div className="alert alert-success" role="alert">
+                                    Successfully joined the group!
+                                </div>
+                            )}
                             <form id="login-form">
                                 <div className="username-group">
                                     <input
@@ -34,7 +47,7 @@ function JoinGroupPage(props : {
                                         placeholder="Group name"
                                         className="group-name-control"
                                         required
-                                        value = {id}
+                                        value={id}
                                         onChange={(event) => setId(event.target.value)}
                                     />
                                 </div>
@@ -45,11 +58,11 @@ function JoinGroupPage(props : {
                                         placeholder="Password"
                                         className="password-control"
                                         required
-                                        value = {password}
+                                        value={password}
                                         onChange={(event) => setPassword(event.target.value)}
                                     />
                                 </div>
-                                <button onClick={e => {e.preventDefault(); onClickedJoin(id, password)}} type="submit" id="login" className="btn btn-primary btn-block">
+                                <button onClick={e => { e.preventDefault(); onClickedJoin(id, password) }} type="submit" id="login" className="btn btn-primary btn-block">
                                     Join
                                 </button>
                             </form>
@@ -58,7 +71,7 @@ function JoinGroupPage(props : {
                         </div>
                     </div>
                     <p className="text-center mt-3">Click on the button below to navigate back to the main page.</p>
-                    <button onClick={e => {e.preventDefault(); props.goToMainPage();}} className="btn btn-secondary btn-block">
+                    <button onClick={e => { e.preventDefault(); props.goToMainPage(); }} className="btn btn-secondary btn-block">
                         Main page
                     </button>
                 </div>
