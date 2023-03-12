819457cd10051f4e7ac5b7ca5fae8241ed48716e
Zakariya00 <100076090+Zakariya00@users.noreply.github.com>
2023-03-12 13:10:36 +0100

    -Z (test Database Setup)

--- server/src/tests/unit.test/db.test.ts
+++ server/src/tests/unit.test/db.test.ts
@@ -1,27 +1,29 @@
 import mongoose, {createConnection} from "mongoose";
+import {conn} from "../../db/model/conn";
+import {restaurantService} from "../../router/restaurant.router";
 
 describe("test-db", () => {
 
     beforeAll(async () => {
-        // Generate a unique database name for testing
-        const testDbName = 'testdb_'; //+ Date.now();
+        await conn.collection('restaurants').deleteMany({});
+        await conn.collection('users').deleteMany({});
+        await conn.collection('groups').deleteMany({});
 
-        // Connect to the MongoDB database with the unique name
-        const conn = createConnection("mongodb+srv://foodmatcher:dat076@dat076.nqicb2v.mongodb.net/test1");
-        //await mongoose.connect("mongodb+srv://foodmatcher:dat076@dat076.nqicb2v.mongodb.net/" + testDbName);
+    });
 
+    afterAll(async () => {
+        await conn.close();
     });
 
     test('should add a new user to the users map', async () => {
-        const conn = createConnection("mongodb+srv://foodmatcher:dat076@dat076.nqicb2v.mongodb.net/test1");
+
+        await restaurantService.createRestaurant("test1", "a", "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+        await restaurantService.createRestaurant("test2", "b", "https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+        await restaurantService.createRestaurant("test3", "c", "https://images.pexels.com/photos/2097090/pexels-photo-2097090.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+        await restaurantService.createRestaurant("test4", "d", "https://images.pexels.com/photos/2641886/pexels-photo-2641886.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+
+
         return true;
     });
-
-// Disconnect from the MongoDB database
-    /*
-    afterAll(async () => {
-        await mongoose.connection.dropDatabase();
-        await mongoose.connection.close();
-    }); */
 
 });
