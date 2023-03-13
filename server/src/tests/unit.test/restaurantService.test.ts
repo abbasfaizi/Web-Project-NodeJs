51d1bdd52bf1d7ab9ba6e90b144095ca4828dfa3
Zakariya00 <100076090+Zakariya00@users.noreply.github.com>
2023-03-14 00:43:11 +0100

    -Z (restaurant.service test finished)

--- server/src/tests/unit.test/restaurantService.test.ts
+++ server/src/tests/unit.test/restaurantService.test.ts
@@ -1,59 +1,97 @@
-
 import {IRestaurantService} from "../../service/restaurant.interface";
 import {makeRestaurantService} from "../../db/service/restaurant.service";
 import {conn} from "../../db/model/conn";
+import {restaurantModel} from "../../db/model/restaurants";
 
-describe("RestaurantService", () => {
+describe("RestaurantService Test", () => {
 
-    const restaurantService = makeRestaurantService();
+    const id : string = "1";
+    const name : string = "Pizza Place";
+    const imageUrl : string = "https://example.com/pizza.jpg";
+    let restaurantService : IRestaurantService;
+
+    beforeAll(async () => {
+        restaurantService = makeRestaurantService();
+    });
 
     beforeEach(async () => {
         await conn.collection('restaurants').deleteMany({});
+    });
+
+    afterEach(async () => {
+        await conn.collection('restaurants').deleteMany({});
         await conn.collection('users').deleteMany({});
         await conn.collection('groups').deleteMany({});
-
     });
 
     afterAll(async () => {
         await conn.close();
     });
 
-    it("should create a new restaurant", async () => {
-      const result = await restaurantService.createRestaurant("1", "Pizza Place", "https://example.com/pizza.jpg");
-      expect(result).toBe(true);
+
+    describe("createRestaurant", () => {
+        it("Should return true on successfully creating a new restaurant", async () => {
+            const success = await restaurantService.createRestaurant(id, name, imageUrl);
+            expect(success).toBe(true);
+
+            const restaurant = await restaurantModel.findOne({ id: id });
+            expect(restaurant).not.toBeNull();
+            expect(restaurant!.name).toBe(name);
+            expect(restaurant!.imageUrl).toBe(imageUrl);
+        });
+
+        it("Should return false on trying to create a restaurant that already exists", async () => {
+            await restaurantModel.create({
+                id: id,
+                name: name,
+                imageUrl: imageUrl,
+            });
+
+            const success = await restaurantService.createRestaurant(id, name, imageUrl);
+            expect(success).toBe(false);
+            const restaurant = await restaurantModel.findOne({ id: id });
+            expect(restaurant).not.toBeNull();
+        });
     });
 
-    it("should return false if restaurant with same ID already exists", async () => {
-      await restaurantService.createRestaurant("1", "Pizza Place", "https://example.com/pizza.jpg");
-      const result = await restaurantService.createRestaurant("1", "Burger Joint", "https://example.com/burger.jpg");
-      expect(result).toBe(false);
+    describe("checkRestaurant", () => {
+        it("Should return true if restaurant exists", async () => {
+            await restaurantModel.create({
+                id: id,
+                name: name,
+                imageUrl: imageUrl,
+            });
+
+            const exists = await restaurantService.checkRestaurant(id);
+            expect(exists).toBe(true);
+        });
+
+        it("Should return false if restaurant doesn't exist", async () => {
+            const exists = await restaurantService.checkRestaurant(id);
+            expect(exists).toBe(false);
+        });
     });
 
-    it("should return true if restaurant with given ID exists", async () => {
-      await restaurantService.createRestaurant("1", "Pizza Place", "https://example.com/pizza.jpg");
-      const result = await restaurantService.checkRestaurant("1");
-      expect(result).toBe(true);
+    describe("getRestaurant", () => {
+        it("Should return restaurant if it exists", async () => {
+            await restaurantModel.create({
+                id: id,
+                name: name,
+                imageUrl: imageUrl,
+            });
+
+            const restaurant = await restaurantService.getRestaurant(id);
+            expect(restaurant).not.toBeUndefined();
+            expect(restaurant.id).toBe(id);
+            expect(restaurant.name).toBe(name);
+            expect(restaurant.imageUrl).toBe(imageUrl);
+        });
+
+        it("Should return undefined if restaurant doesn't exist", async () => {
+            const restaurant = await restaurantService.getRestaurant(id);
+            expect(restaurant).toBeUndefined();
+        });
+
     });
 
-    it("should return false if restaurant with given ID doesn't exist", async () => {
-      const result = await restaurantService.checkRestaurant("should-not-exist");
-      expect(result).toBe(false);
-    });
-
-    it("should return the correct restaurant for the given ID", async () => {
-      await restaurantService.createRestaurant("1", "Pizza Place", "https://example.com/pizza.jpg");
-      const result = await restaurantService.getRestaurant("1");
-      expect(result.id).toBe("1");
-      expect(result.name).toBe("Pizza Place");
-      expect(result.imageUrl).toBe("https://example.com/pizza.jpg");
-    });
-
-    it("should return all stored restaurants", async () => {
-      await restaurantService.createRestaurant("1", "Pizza Place", "https://example.com/pizza.jpg");
-      await restaurantService.createRestaurant("2", "Burger Joint", "https://example.com/burger.jpg");
-      const result = await restaurantService.getRestaurants();
-      expect(result.size).toBe(2);
-      expect(result.get("1")).toBeDefined();
-      expect(result.get("2")).toBeDefined();
-    });
 });
